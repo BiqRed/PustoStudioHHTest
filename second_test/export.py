e4575f0e8e085a6eb4c9a15005ff1936a99982d1
Nikita <82651186+BiqRed@users.noreply.github.com>
2024-10-03 02:14:42 +0700

    Add export

---
+++ second_test/export.py
@@ -0,0 +1,24 @@
+import csv
+
+from django.db.models import QuerySet
+
+
+def export_to_cvs(queryset: QuerySet) -> None:
+    """Export PlayerLevel queryset to csv file."""
+
+    with open('player_levels.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Player ID', 'Level', 'Is Completed', 'Prize'])
+
+        for player_level in queryset:
+            prize_title = None
+            player_level_prize = player_level.received_prize
+            if player_level_prize:
+                prize_title = player_level_prize.prize.title
+
+            writer.writerow([
+                player_level.player.player_id,
+                player_level.level.title,
+                player_level.is_completed,
+                prize_title or ''
+            ])
